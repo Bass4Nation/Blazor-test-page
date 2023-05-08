b1666cf094049e8b144722edf2d2d42f9254cd84
Kristoffer Snopestad Søderkvist <55276856+Bass4Nation@users.noreply.github.com>
2023-05-08 16:01:36 +0200

    Added a lot

---
+++ Models/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace web_test_b4n.Models
+{
+	public class Person
+	{
+		public string Name { get; set; }
+		public string JobTitle { get; set; }
+		public List<string> About { get; set; }
+		public string ImageUrl { get; set; }
+
+	}
+}
